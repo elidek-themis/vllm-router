b0ae9c19e5e98ac4e872b140423bc875389f3f7b
karanikolopoulos <ckaranikolopoulos@gmail.com>
2026-03-20 20:41:25 +0000

    chore: update vLLM

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import requests
 
+HTTP_OK = 200
+
 
 class Request:
     @staticmethod
@@ -12,13 +14,6 @@ class Request:
     @staticmethod
     def model(port: int) -> requests.Response:
         url = f"http://localhost:{port}/v1/models"
-        r = requests.get(url)
-        r.raise_for_status()
-        return r
-
-    @staticmethod
-    def status(port: int) -> requests.Response:
-        url = f"http://localhost:{port}/is_sleeping"
         r = requests.get(url)
         r.raise_for_status()
         return r
